38f32c35740b8a398012137ece7639baac5bfba7
AlonsoAR <alonso19_82@hotmail.com>
2021-02-15 20:03:19 +0100

    Este es un mensaje para explicar lo que hemos hecho

---
+++ src/PEED4/Main.java
@@ -0,0 +1,56 @@
+package PEED4;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+
+		 Emonedero mimonedero;
+
+	        double dinero;
+
+	        double saldo_disponible = global_monedero(1000);
+
+	        System.out.println("El efectivo del monedero es  "+ saldo_disponible );
+
+	    }
+
+	public static double global_monedero(double dinero) {
+		Emonedero mimonedero;
+		mimonedero = new Emonedero("Nombre y apellidos del Alumno",500, "dni del alumno");
+
+		try 
+
+		{
+
+		    mimonedero.pagar(200);
+
+		} catch (Exception e)
+
+		{
+
+		    System.out.print("No se puede pagar  ");
+
+		}
+
+		try
+
+		{
+
+		    System.out.println("Debes recargar monedero");
+
+		    mimonedero.recargar(300);
+
+		} catch (Exception e)
+
+		{
+
+		    System.out.print("Error al recargar");
+
+		}
+
+		double saldo_disponible = mimonedero.efectivo();
+		return saldo_disponible;
+	}
+
+}
